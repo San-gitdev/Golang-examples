db6fe6f9c602521565f555d7fd308cff530750aa
San-gitdev <30860598+San-gitdev@users.noreply.github.com>
2020-12-27 16:58:44 +0530

    Update main.go

--- Cards/main.go
+++ Cards/main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-  var card string = "Ace of Spades"
-  fmt.println(card)
+func main() {
+	var card string = newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+
+	return "Five of Diamonds"
+
 }
