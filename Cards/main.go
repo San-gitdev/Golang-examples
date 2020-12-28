04037ba505947206cc244ff457603ac6effe6175
San-gitdev <30860598+San-gitdev@users.noreply.github.com>
2020-12-28 11:09:07 +0530

    receiver function
    Reciever function added in deck.go
    Main.go refactored to call that function.

--- Cards/main.go
+++ Cards/main.go
@@ -1,16 +1,10 @@
 package main
 
-import "fmt"
 
 func main() {
-	cards := []string{newCard(), "Ace of Diamonds"}
-	fmt.Println(cards)
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Five of Spade")
-	fmt.Println(cards)
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
+	cards.print()
 }
 
 func newCard() string {
