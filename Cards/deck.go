e1b98ebe3d9057b06f0a03aeb18fff34cafa22d2
San-gitdev <30860598+San-gitdev@users.noreply.github.com>
2020-12-28 16:51:30 +0530

    SaveToFile

--- Cards/deck.go
+++ Cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "strings"
+  "io/ioutil"
 )
 //Create a new type of deck
 //which is a slice of strings
@@ -33,7 +34,12 @@ return d[:handSize], d[handSize:]
 
 }
 
-
 func (d deck) toString() string{
 return	strings.Join([]string(d), ",")
 }
+
+func (d deck) saveToFile(filename string) error {
+
+return  ioutil.WriteFile(filename, []byte(d.toString()),0666)
+
+  }
--- Cards/main.go
+++ Cards/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	cards := newDeck()
-fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
 
 func newCard() string {
